c69cad717c56a18b1066d213dd0760da56bd4d03
joshkery-rlmg <josh@rlmg.com>
2026-05-07 09:21:23 -0400

    First Commit

---
+++ Samples/Config Example/ExampleConfigData.cs
@@ -0,0 +1,13 @@
+namespace rlmg.Tools.ContentLoading.Examples
+{
+    using UnityEngine;
+
+    [System.Serializable]
+    public class ConfigData
+    {
+        public int attractTimeout = 30;
+
+        public Vector2 screenResolution = new Vector2(1920, 1080);
+    }
+
+}
